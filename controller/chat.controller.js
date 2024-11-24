59ee5b4e38805f8952b40c3faa2b548bbcad78b7
[anudeep avula] <[anudeepavula009@gmail.com]>
2024-11-24 10:43:48 +0530

    --minor bugs resolved

--- controller/chat.controller.js
+++ controller/chat.controller.js
@@ -1,4 +1,5 @@
 import { Chat } from "../models/chat.model.js";
+import mongoose from "mongoose";
 
 export const saveMessage = async (req, res) => {
   const { senderId, recipientId, message } = req.body;
@@ -6,6 +7,7 @@ export const saveMessage = async (req, res) => {
   try {
     const chatMessage = new Chat({ senderId, recipientId, message });
     const savedMessage = await chatMessage.save();
+    console.log(savedMessage)
     res.status(201).json({ message: "Message saved", data: savedMessage });
   } catch (error) {
     res
@@ -15,17 +17,31 @@ export const saveMessage = async (req, res) => {
 };
 
 export const getMessages = async (req, res) => {
-  const { senderId, recipientId } = req.query;
+  const { senderId, recipientId } = req.body;
+ 
+  if (!senderId || !recipientId) {
+    return res
+      .status(400)
+      .json({ message: "senderId and recipientId are required" });
+  }
 
   try {
     const messages = await Chat.find({
       $or: [
-        { senderId, recipientId },
-        { senderId: recipientId, recipientId: senderId },
+        {
+          senderId: new mongoose.Types.ObjectId(`${senderId}`),
+          recipientId: new mongoose.Types.ObjectId(`${recipientId}`),
+        },
+        {
+          senderId: new mongoose.Types.ObjectId(`${recipientId}`),
+          recipientId: new mongoose.Types.ObjectId(`${senderId}`),
+        },
       ],
     }).sort({ timestamp: 1 });
+
     res.status(200).json(messages);
   } catch (error) {
+    console.error("Error fetching messages:", error);
     res
       .status(500)
       .json({ message: "Error fetching messages", error: error.message });
